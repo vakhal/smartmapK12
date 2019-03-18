3543f1231e57eb3dbdcdde68e1108f4ab0e677bd
Артем Д. Шведенко <shvedenko@infomatika.local>
2019-03-18 11:58:22 +0300

    cameras

--- views/camera.php
+++ views/camera.php
@@ -1,8 +1,9 @@
 <?php
 ?>
 <div class="camera">
-    <div class="popup__bg"></div>
+
     <div class="camera__content">
         <h4>Этаж №<?=$_POST['floor']?>. Камера №<?=$_POST['id']?></h4>
+        <iframe style="width:640px; height:480px;" allowfullscreen src="http://80.250.183.245:8080/10-34ff7d052b/embed.html"></iframe>
     </div>
 </div>
